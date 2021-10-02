b68f135d9ad4372e6c8acca9fcb50ab308633f1b
k2 sl <kiyanosh.s@gmail.com>
2021-10-03 02:39:59 +0330

    add contrller and model

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::get('/produtcs', 'App/Http/Controller/ProductController@index');
+Route::post('/produtcs','App/Http/Controller/ProductController@store'); 
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request){
     return $request->user();
 });
